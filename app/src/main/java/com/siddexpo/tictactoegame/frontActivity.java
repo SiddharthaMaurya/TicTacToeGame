8d8c8e84b8c6ff2bf92252c0e1b00ebf7d1493ca
SiddharthaMaurya <siddharthamaurya496@gmail.com>
2026-06-06 15:27:35 +0530

    First Version

---
+++ app/src/main/java/com/siddexpo/tictactoegame/frontActivity.java
@@ -0,0 +1,41 @@
+package com.siddexpo.tictactoegame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class frontActivity extends AppCompatActivity {
+
+    Button btnFront;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_front);
+
+        btnFront = findViewById(R.id.btnFront);
+
+        Intent play = new Intent(frontActivity.this, MainActivity.class);
+
+        btnFront.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(play);
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ app/src/main/java/com/siddexpo/tictactoegame/winnerActivity.java
@@ -0,0 +1,54 @@
+package com.siddexpo.tictactoegame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
+
+import java.nio.channels.InterruptedByTimeoutException;
+
+public class winnerActivity extends AppCompatActivity {
+
+    TextView gameResult;
+    Button btnAgain;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_winner);
+
+        gameResult = findViewById(R.id.result);
+
+        btnAgain = findViewById(R.id.btnAgain);
+
+
+        String winner = getIntent().getStringExtra("winner");
+        gameResult.setText("Winner is: "+winner);
+
+        Intent back = new Intent(winnerActivity.this,MainActivity.class);
+        btnAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(back);
+            }
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
